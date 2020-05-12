74031ab8a7774785d4519e8f8d2796a06cb22ba0
ejaramillom <ejaramilo@hotmail.com>
2020-05-12 10:37:25 -0500

    modified styles and favicon definition

--- src/App.js
+++ src/App.js
@@ -2,77 +2,34 @@ import React from "react";
 import "./App.css";
 import { Button, Container, Row, Col } from "react-bootstrap";
 import Posts from "./components/Posts";
-
-const datas = [
-  {
-    id: 1,
-    title: "Manejo de dependencias en Ruby con Bundler",
-    description:
-      "Bundler es una manejador de dependencias para Ruby. Aunque viene incluido con Rails, Bundler no es exclusivo de Rails, lo puedes usar para manejar las dependencias de cualquier proyecto de Ruby.",
-    url:
-      "http://blog.makeitreal.camp/manejo-de-dependencias-en-ruby-con-bundler/",
-    votes: 42,
-    writer_avatar_url:
-      "//a.disquscdn.com/uploads/users/2864/1155/avatar92.jpg?1481303405",
-    post_image_url:
-      "http://blog.makeitreal.camp/assets/images/bg-images/bundler.jpg"
-  },
-  {
-    id: 2,
-    title: "Descubre si Make it Real es para ti",
-    description:
-      "En Make it Real buscamos entrenar a los desarrolladores Web que nosotros mismos quisiéramos contratar. Personas con autodisciplina que sean capaces de resolver problemas complejos y se adapten rápidamente a nuevas tecnologías y escenarios. En este post vamos a discutir algunas características de nuestro programa para que descubras si Make it Real es para ti.",
-    url: "http://blog.makeitreal.camp/descubre-si-make-it-real-es-para-ti/",
-    votes: 43,
-    writer_avatar_url:
-      "//a.disquscdn.com/uploads/users/2864/1155/avatar92.jpg?1481303405",
-    post_image_url:
-      "http://blog.makeitreal.camp/assets/images/bg-images/laptop-sublime.jpg"
-  },
-  {
-    id: 3,
-    title: "¿Qué es código?",
-    description:
-      "Semáforos, automóviles, aviones, aeropuertos, satélites, el sistema financiero, gran parte de nuestras vidas depende del código que varias generaciones de programadores han escrito. Pero ¿qué es código? ¿quién lo ejecuta y cómo? En este post vamos a hacer un recorrido histórico para entender cómo es que la electricidad se convierte en código y cómo surgieron los lenguajes de programación.",
-    url: "http://blog.makeitreal.camp/que-es-codigo/",
-    votes: 44,
-    writer_avatar_url:
-      "//a.disquscdn.com/uploads/users/2864/1155/avatar92.jpg?1481303405",
-    post_image_url:
-      "http://blog.makeitreal.camp/assets/images/bg-images/code.jpg"
-  },
-  {
-    id: 4,
-    title: "Aprende Desarrollo Web gratis",
-    description:
-      "¿Quieres iniciar en el mundo del desarrollo Web y no sabes por dónde empezar? Conoce Aprende Desarrollo Web, un curso completamente gratis dirigido a personas sin experiencia en el que aprenderás a crear y publicar sitios interactivos en Internet con HTML, CSS y JavaScript.",
-    url: "http://blog.makeitreal.camp/aprende-desarrollo-web-gratis/",
-    votes: 45,
-    writer_avatar_url:
-      "//a.disquscdn.com/uploads/users/2864/1155/avatar92.jpg?1481303405",
-    post_image_url:
-      "http://blog.makeitreal.camp/assets/images/bg-images/aprende-desarrollo-web-bg.png"
-  }
-];
+import datas from './components/datas'
 
 class App extends React.Component {
-  state = { data: datas, order: "asc" };
-  _handleVote = (postId, type) => {
+
+  constructor( props ){
+    super( props );
+    this.state = {
+      data: datas,
+      order: "ascendant"
+    };
+  }
+
+  orderPosts = ( postId, type ) => {
     const updatedList = this.state.data.map(post => {
-      if (post.id === postId && type === "Up") {
+      if ( post.id === postId && type === "Up" ) {
         return { ...post, votes: post.votes + 1 };
-      } else if (post.id === postId && type === "Down") {
+      } else if ( post.id === postId && type === "Down" ) {
         return { ...post, votes: post.votes - 1 };
       } else {
         return post;
       }
     });
 
-    if (this.state.order === "asc") {
-      updatedList.sort((a, b) => (a.votes > b.votes ? 1 : -1));
+    if ( this.state.order === "ascendant" ) {
+      updatedList.sort(( a, b ) => ( a.votes > b.votes ? 1 : -1 ));
     }
-    if (this.state.order === "desc") {
-      updatedList.sort((a, b) => (a.votes < b.votes ? 1 : -1));
+    if ( this.state.order === "descendant" ) {
+      updatedList.sort(( a, b ) => ( a.votes < b.votes ? 1 : -1 ));
     }
     this.setState({
       data: updatedList
@@ -80,49 +37,49 @@ class App extends React.Component {
   };
 
   componentWillMount() {
-    this._handleOrder("asc");
+    this.handleOrder( "ascendant" );
   }
 
-  _handleOrder = order => {
+  handleOrder = order => {
     const newOrder = this.state.data;
-    if (order === "asc") {
-      newOrder.sort((a, b) => (a.votes > b.votes ? 1 : -1));
+    if ( order === "ascendant" ) {
+      newOrder.sort(( a, b ) => ( a.votes > b.votes ? 1 : -1 ));
     }
-    if (order === "desc") {
-      newOrder.sort((a, b) => (a.votes < b.votes ? 1 : -1));
+    if (order === "descendant") {
+      newOrder.sort(( a, b ) => ( a.votes < b.votes ? 1 : -1 ));
     }
     this.setState({ data: newOrder, order: order });
   };
 
   render() {
-    console.log(this.state.order);
+    console.log( this.state.order );
     const { data, order } = this.state;
     return (
       <Container>
-        <h2 className='text-center'>Blog posts Populares</h2>
         <Row>
-          <Col md={{ span: 6, offset: 2 }}>
-            <div>
-              Orden:
+          <Col md={ 12 } className='text-center'>
+          <h1 className='text-center'>BLOG | POST POPULARES </h1>
+          </Col>
+          <Col xs={ 8 } className='text-center'>
+            <div className='center-buttons'>
+              Ordenar:
               <Button
                 variant="outline-primary"
-                className={order === "asc" ? "active" : ""}
-                onClick={this._handleOrder.bind(this, "asc")}
-                style={{ marginLeft: 10 }}
-              >
+                className={ 'margin-button' + order === "ascendant" ? "active" : "" }
+                onClick={ this.handleOrder.bind(this, "ascendant" )}
+                style={{ marginLeft: 10 }} >
                 Ascendente
               </Button>
               <Button
                 variant="outline-primary"
-                className={order === "desc" ? "active" : ""}
-                onClick={this._handleOrder.bind(this, "desc")}
-                style={{ marginLeft: 10 }}
-              >
+                className= { 'margin-button' + order === "descendant" ? "active" : ""}
+                onClick={ this.handleOrder.bind(this, "descendant" )}
+                style={{ marginLeft: 10 }}>
                 Descendente
               </Button>
             </div>
           </Col>
-          <Posts data={data} onVote={this._handleVote} />
+          <Posts data={ data } onVote={ this.orderPosts } />
         </Row>
       </Container>
     );
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,45 +1,49 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
+
 class Post extends Component {
-  _handleClickUp = () => {
-    this.props.onVote(this.props.post.id, "Up");
+  handleClickUp = () => {
+    this.props.onVote( this.props.post.id, "Up" );
   };
-  _handleClickDown = () => {
-    this.props.onVote(this.props.post.id, "Down");
+  handleClickDown = () => {
+    this.props.onVote( this.props.post.id, "Down" );
   };
   render() {
     const { post } = this.props;
     return (
-      <Row key={post.id}>
-        <Col sm={{ span: 3, offset: 2 }}>
-          <img src={post.post_image_url} alt={post.title}></img>
+      <Row key={ post.id }>
+        <Col sm={{ span:3, offset: 2 }}>
+          <img
+            src={ post.post_image_url }
+            alt={ post.title }>
+          </img>
         </Col>
-        <Col sm={1}>
+        <Col sm={ 1 }>
           <div className="votes">
             <span>
               <button
-                onClick={this._handleClickUp}
-                className="fas fa-caret-up"
+                onClick={ this.handleClickUp }
+                className="fas fa-angle-up"
               />
-              <div>{post.votes}</div>
+              <div>{ post.votes }</div>
               <button
-                onClick={this._handleClickDown}
-                className="fas fa-caret-down"
+                onClick={ this.handleClickDown }
+                className="fas fa-angle-down"
               />
             </span>
           </div>
         </Col>
-        <Col sm={5}>
+        <Col sm={ 5 }>
           <div>
             <h4>
-              <a href={post.url}>{post.title}</a>
+              <a href={ post.url }>{ post.title }</a>
             </h4>
-            {post.description}
+            { post.description }
           </div>
           <div className="author">
             <span>
               <em>Escrito por: &nbsp; </em>
-              <img className="avatar" src={post.writer_avatar_url} />{" "}
+              <img className="avatar" src={ post.writer_avatar_url } alt="Avatar"/>{" "}
             </span>
           </div>
         </Col>
